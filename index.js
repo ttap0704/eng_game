49d247eb9de6d4a2320036bc66a9f0eaea1f6570
LeeDaeGyu <ttap0704@naver.com>
2022-04-04 13:53:52 +0900

    완료

--- index.js
+++ index.js
@@ -1,3 +1,80 @@
+// 게임 진행
+// 1. 선이 이동할 수 있는 경로의
+// 2. 맞는 뜻, 단어를 클릭하여 정답을 맞춤
+// 3. 더 이상 맞출 수 있는 정답이 없을 경우, 또는 모든 문제를 맞출 경우 처음부터 다시 시작
+
+// 게임 규칙
+// 1. 선은 2번 이상 꺾을 수 없음
+// 2. 붙어 있는 맞는 뜻, 단어는 정답으로 인정
+
+// 알고리즘 설명 (함수)
+// ** shuffle **
+//  - 문제의 수만큼 랜덤 수를 만들어 배열로 반환
+// ** makeQuestionArray **
+//  - shuffle에서 받은 배열을 토대로 문제를 랜덤으로 만듬
+//  - 각 첫 번째, 마지막 열과 행은 *로 표기됨
+//  - 여기서 *은 이동할 수 있는 경로
+// ** checkPossibleQuetions **
+//  - 만든 문제를 검증하여 맞출 수 있는 경우의 수를 반환
+//  - 처음 문제를 만들 때, 초기에 맞출 수 있는 정답의 수를 정하여, 문제를 재설정 하는데 사용
+//  - 문제를 맞출 때 마다 검증하여, 맞출 수 있는 문제의 수를 알 수 있음
+// ** checkAnswer **
+//  - 첫 번째, 두 번째 클릭한 단어와 뜻이 정답인지 체크하는 함수
+// ** findCourse **
+//  - 정답의 경로를 찾는 함수
+//  - [오른쪽, 왼쪽, 아래, 위] 순서대로 탐색하여, 해당 지점이 '*'일 시, 자신을 다시 호출하는 재귀함수
+//    - 다시 호출할 때 x, y값을 새롭게 전달하여 자동으로 다음 지점을 방문할 수 있도록 설정
+//    - 해당 지점이 '*'이라면 계속 탐색, 아니라면 정답 유무를 판단하여
+//    - 정답이라면 이동 경로 배열을 반환, 아니라면 false를 반환
+//    - 세 번이상 꺾일 경우에도 false를 반환
+//    - 각 경로에서 하나라도 이동 경로를 반환한다면 정답으로 인정, 아니라면 false를 반환하여 정답 실패 처리
+//  - 경로가 있으면 바로 반환하기 때문에, 최단 경로는 보장할 수 없음
+// ** setRightAnswer **
+//  - findCourse에서 경로를 반환 했을 시, view에 이동 한 경로를 출력해 줌
+//  - 경로 출력 후 해당 지점을 *로 치환하고 새로운 문제 배열을 반환하여 문제를 재설정
+
+// 6x4 or 4x6
+// 처음 설정 > 3
+// const arr = [
+//   {word: "사과", mean: "apple"},
+//   {word: "더하기", mean: "add"},
+//   {word: "컵", mean: "cup"},
+//   {word: "마우스", mean: "mouse"},
+//   {word: "남자", mean: "man"},
+//   {word: "여자", mean: "woman"},
+//   {word: "소년", mean: "boy"},
+//   {word: "소녀", mean: "girl"},
+//   {word: "가족", mean: "family"},
+//   {word: "집", mean: "home"},
+//   {word: "가다", mean: "go"},
+//   {word: "뒤", mean: "back"},
+// ];
+
+// 6x6
+// 처음 설정 > 7
+// const arr = [
+//   {word: "사과", mean: "apple"},
+//   {word: "더하기", mean: "add"},
+//   {word: "컵", mean: "cup"},
+//   {word: "마우스", mean: "mouse"},
+//   {word: "남자", mean: "man"},
+//   {word: "여자", mean: "woman"},
+//   {word: "소년", mean: "boy"},
+//   {word: "소녀", mean: "girl"},
+//   {word: "가족", mean: "family"},
+//   {word: "집", mean: "home"},
+//   {word: "가다", mean: "go"},
+//   {word: "뒤", mean: "back"},
+//   {word: "핸드폰", mean: "phone"},
+//   {word: "커피", mean: "coffee"},
+//   {word: "의자", mean: "chair"},
+//   {word: "침대", mean: "bed"},
+//   {word: "코트", mean: "coat"},
+//   {word: "바지", mean: "pants"},
+// ];
+
+// 8x6 or 6x8
+// 처음 설정 > 9
 const arr = [
   {word: "사과", mean: "apple"},
   {word: "더하기", mean: "add"},
@@ -11,6 +88,18 @@ const arr = [
   {word: "집", mean: "home"},
   {word: "가다", mean: "go"},
   {word: "뒤", mean: "back"},
+  {word: "핸드폰", mean: "phone"},
+  {word: "커피", mean: "coffee"},
+  {word: "의자", mean: "chair"},
+  {word: "침대", mean: "bed"},
+  {word: "코트", mean: "coat"},
+  {word: "바지", mean: "pants"},
+  {word: "신발", mean: "shoes"},
+  {word: "냉장고", mean: "refrigerator"},
+  {word: "강아지", mean: "dog"},
+  {word: "고양이", mean: "cat"},
+  {word: "대통령", mean: "president"},
+  {word: "병원", mean: "hospital"},
 ];
 
 let answer = [];
@@ -35,17 +124,9 @@ function shuffle(array) {
   return array;
 }
 
-function getEmptyArray(leng) {
-  let arr = [];
-  for (i = 0; i < leng + 2; i++) {
-    arr.push("*");
-  }
-
-  return arr;
-}
-
 function makeQuestionArray(arr, random_keys_arr, max_y, max_x) {
   let quetion_arr = [];
+  console.log(random_keys_arr);
   for (let i = 0, leng = max_x + 2; i < leng; i++) {
     let row = [];
     for (let j = 0, jleng = max_y + 2; j < jleng; j++) {
@@ -185,11 +266,22 @@ function setRightAnswer(move_arr) {
   question[answer_coordinate[0][0]][answer_coordinate[0][1]] = "*";
   question[answer_coordinate[1][0]][answer_coordinate[1][1]] = "*";
   check_count = checkPossibleQuetions(question, answer_arr);
+  console.log(check_count);
   setTimeout(() => {
     if (ok_cnt == arr.length) {
       alert("정답을 모두 맞혔습니다!");
+      const restart = confirm("다시 진행 하시겠습니까?");
+      if (restart) {
+        init(arr, 8, 6);
+      }
     } else {
       alert("정답입니다!");
+      if (check_count == 0 && ok_cnt < arr.length) {
+        const restart = confirm("맞출 수 있는 정답이 없습니다.\r\n다시 시작 하시겠습니까?");
+        if (restart) {
+          init(arr, 8, 6);
+        }
+      }
     }
     setQuestion();
   }, 250);
@@ -233,13 +325,15 @@ function setQuestion() {
 
 function init(arr, max_y, max_x) {
   let check_count = 0;
-  while (!(check_count > 3)) {
+  while (!(check_count > 9)) {
     const random_keys_arr = shuffle([...Array(arr.length * 2).keys()]);
     const question_res = makeQuestionArray(arr, random_keys_arr, max_y, max_x);
     question = question_res.quetion_arr;
     const check_arr = question_res.answer_arr;
     check_count = checkPossibleQuetions(question, check_arr);
+    console.log(check_count);
   }
+  alert("시작하세요!");
   setQuestion();
 }
 
@@ -442,4 +536,4 @@ function onClickTarget(x, y, question_number) {
   }, 0);
 }
 
-init(arr, 6, 4);
+init(arr, 8, 6);
